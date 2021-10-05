bd6954b066f7d08c4754cc5e72e046775d2344d2
bailey-97 <baileyjallison@gmail.com>
2021-10-05 20:51:42 -0300

    Update cephfs-snapshot-tool.py

--- cephfs-snapshot-tool.py
+++ cephfs-snapshot-tool.py
@@ -19,24 +19,42 @@ from optparse import OptionParser
 def queryCephFSmounts():
     try:
         cephfsMountChecks = subprocess.check_output("df -PTh | awk '{print($7, $2)'} | grep ceph",shell=True, encoding='utf=8')
+##if no mounts are found exit
     except subprocess.CalledProcessError:
-        print("no cephfs mounts found")
-        
+        do: sys.exit()
+
+
+#################################################################################
+# validate cephfs mount point and query where snaps would be taken
+#################################################################################
+    else:
         pathToDirQuery=input("Path to CephFS dir where snapshots should be taken: ")
         df_pathtocephfs = subprocess.Popen(['df', '-PTh', pathToDirQuery], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         awk_for_ceph = subprocess.Popen(['awk', '{print $2}'], stdin=df_pathtocephfs.stdout, stdout=subprocess.PIPE, universal_newlines=True)
         df_pathtocephfs.stdout.close()
         is_it_ceph, err = awk_for_ceph.communicate()
+##if cephfs directory validate
     if "ceph" in is_it_ceph:
-            dayTimeVar = subprocess.check_output(['date', '+%Y-%m-%d_%H%M%S'], universal_newlines=True).strip()
-            if pathToDirQuery.endswith("/"):
-                mkdir_snap = subprocess.check_output(['mkdir', f"{pathToDirQuery}"+'.snap/'+f"{pathToDirQuery.split('/')[-2]}"+f"-{dayTimeVar}"])
-            elif pathToDirQuery.endswith(""):
-                mkdir_snap = subprocess.Popen(['mkdir', f"{pathToDirQuery}"+'/.snap/'+f"{pathToDirQuery.rsplit('/')[-1]}"+f"-{dayTimeVar}"])
-                print(pathToDirQuery.rsplit('/')[-1])
+        print("yes, good choice")
+##if not cephfs dir validate
     elif "ceph" not in is_it_ceph:
-            do: sys.exit()
-    print("yo whatup print check")
+        print("not a valid cephfs directory")
+        do: sys.exit()
+
+
+###############################################################################
+# manual cephfs snaps
+###############################################################################
+
+####What would you like your snapshot task(s) to be
+##Manual Snapshots
+    dayTimeVar = subprocess.check_output(['date', '+%Y-%m-%d_%H%M%S'], universal_newlines=True).strip()
+    if pathToDirQuery.endswith("/"):
+        mkdir_snap = subprocess.check_output(['mkdir', f"{pathToDirQuery}"+'.snap/'+f"{pathToDirQuery.split('/')[-2]}"+f"-{dayTimeVar}"])
+    elif pathToDirQuery.endswith(""):
+        mkdir_snap = subprocess.Popen(['mkdir', f"{pathToDirQuery}"+'/.snap/'+f"{pathToDirQuery.rsplit('/')[-1]}"+f"-{dayTimeVar}"])
+        print(pathToDirQuery.rsplit('/')[-1])
+
 ###############################################################################
 # manual cephfs snaps
 ###############################################################################

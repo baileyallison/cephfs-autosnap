0039a54cc901619b1a03eddb744e1c361ec0c531
bailey-97 <baileyjallison@gmail.com>
2021-10-06 19:12:38 -0300

    Update cephfs-snapshot-tool.py

--- cephfs-snapshot-tool.py
+++ cephfs-snapshot-tool.py
@@ -55,7 +55,7 @@ def pathofCephFS_snaps():
         dayTimeVar = subprocess.check_output(['date', '+%Y-%m-%d_%H%M%S'], universal_newlines=True).strip()
         if pathToDirQuery.endswith("/"):
             mkdir_snap = subprocess.check_output(['mkdir', f"{pathToDirQuery}"+'.snap/'+f"{pathToDirQuery.split('/')[-2]}"+f"-{dayTimeVar}"])
-            print("Snapshot created:"+str(mkdir_snap))
+            print("Snapshot created:"+(pathToDirQuery.rsplit('/')[-1])+"-"+(dayTimeVar))
         elif pathToDirQuery.endswith(""):
             mkdir_snap = subprocess.check_output(['mkdir', f"{pathToDirQuery}"+'/.snap/'+f"{pathToDirQuery.rsplit('/')[-1]}"+f"-{dayTimeVar}"])
             print("Snapshot created:"+(pathToDirQuery.rsplit('/')[-1])+"-"+(dayTimeVar))
